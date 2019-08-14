3d7794609bc2dfd237c06f6f08e92f2229f0b2ea
Tim <timbbby@gmail.com>
2019-08-14 13:17:21 +1200

    Bad camera movement

---
+++ Prototype3/Assets/Scripts/Friend.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FriendState
+{
+    WaitingForFriend,
+    Moving,
+    AtHangi
+}
+
+public class Friend : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private void Update()
+    {
+        
+    }
+
+
+}
---
+++ Prototype3/Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float rotationSpeed = 1.0f;
+    public Transform target, player;
+    float mouseX, mouseY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+    private void LateUpdate()
+    {
+        CamControl();
+    }
+
+    void CamControl()
+    {
+        mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
+        // mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
+        // mouseY = Mathf.Clamp(mouseY, -35, 60);
+
+        transform.LookAt(target);
+
+        target.rotation = Quaternion.Euler(0.0f, mouseX, 0.0f);
+        player.rotation = Quaternion.Euler(0.0f, mouseX, 0.0f);
+    }
+}
