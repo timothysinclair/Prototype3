d025ad82dcc1a9fefc9699b19823a880d1c7e708
Tim Sinclair <timbbby@gmail.com>
2019-08-16 16:53:32 +1200

    Changed player movement to use rigidbody

--- Prototype3/Assets/Scripts/Player/Player.cs
+++ Prototype3/Assets/Scripts/Player/Player.cs
@@ -8,7 +8,6 @@ public class Player : MonoBehaviour
     public Transform cameraTransform;
     public float gravity = 15.0f;
     public float rotationSpeed = 240.0f;
-    public float jumpForce = 10.0f;
 
     [Range(0.01f, 25.0f)]
     public float speed = 5.0f;
@@ -23,87 +22,48 @@ public class Player : MonoBehaviour
 
     private Vector3 cameraOffset;
     private Vector3 movementDirection = Vector3.zero;
-    private CharacterController characterController;
 
     private Vector3 force = Vector3.zero;
 
     private void Start()
     {
         cameraOffset = (cameraTransform.position - transform.position) + new Vector3(2.0f, 0.0f, 0.0f);
-        characterController = GetComponent<CharacterController>();
 
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
+        
     }
 
     private void Update()
     {
         // Get Input for axis
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            AddForce(Vector3.up * jumpForce);
-        }
+        // float horizontal = Input.GetAxis("Horizontal");
+        // float vertical = Input.GetAxis("Vertical");
 
         // Calculate the forward vector
-        Vector3 forwardDirection = Vector3.Scale(cameraTransform.forward, new Vector3(1, 0, 1)).normalized;
-        Vector3 movement = vertical * forwardDirection + horizontal * cameraTransform.right;
+        // Vector3 forwardDirection = Vector3.Scale(cameraTransform.forward, new Vector3(1, 0, 1)).normalized;
+        // Vector3 movement = vertical * forwardDirection + horizontal * cameraTransform.right;
 
-        if (movement.magnitude > 1f) movement.Normalize();
-        movement = transform.InverseTransformDirection(movement);
+        // if (movement.magnitude > 1f) movement.Normalize();
+        // movement = transform.InverseTransformDirection(movement);
 
-        float turnAmount = Mathf.Atan2(movement.x, movement.z);
-        transform.Rotate(0, turnAmount * rotationSpeed * Time.deltaTime, 0);
-
-        if (characterController.isGrounded)
-        { 
-            movementDirection = transform.forward * movement.magnitude;
-            movementDirection *= speed;
-        }
-
-        movementDirection.y -= gravity * Time.deltaTime;
-        characterController.Move(movementDirection * Time.deltaTime);
-
-        UpdateForce(Time.deltaTime);
+        // float turnAmount = Mathf.Atan2(movement.x, movement.z);
+        // transform.Rotate(0, turnAmount * rotationSpeed * Time.deltaTime, 0);
     }
      
     private void LateUpdate()
     {
-        if (rotateAroundPlayer)
-        {
-            float xAxis = Input.GetAxis("Mouse X");
-            Quaternion turnAngle = Quaternion.AngleAxis(xAxis* cameraRotationSpeed, Vector3.up);
+        //if (rotateAroundPlayer)
+        //{
+        //    float xAxis = Input.GetAxis("Mouse X");
+        //    Quaternion turnAngle = Quaternion.AngleAxis(xAxis * cameraRotationSpeed, Vector3.up);
 
-            cameraOffset = turnAngle * cameraOffset;
-        }
+        //    cameraOffset = turnAngle * cameraOffset;
+        //}
 
-        Vector3 position = transform.position + cameraOffset;
-        cameraTransform.position = Vector3.Slerp(cameraTransform.position, position, smoothing);
+        //Vector3 position = transform.position + cameraOffset;
+        //cameraTransform.position = Vector3.Slerp(cameraTransform.position, position, smoothing);
 
-        if (lookAtPlayer || rotateAroundPlayer) cameraTransform.LookAt(transform);
-    } 
+        //if (lookAtPlayer || rotateAroundPlayer) cameraTransform.LookAt(this.transform);
 
-    public void AddForce(Vector3 newForce)
-    {
-        force += newForce;
-    }
-
-    private void UpdateForce(float deltaTime)
-    {
-        if (force.magnitude > 4.0f)
-        {
-            characterController.Move(force * deltaTime);
-        }
-        if (characterController.isGrounded)
-        {
-            force = Vector3.Lerp(force, Vector3.zero, 5.0f * deltaTime);
-        }
-        else
-        {
-            force = Vector3.Lerp(force, Vector3.zero, 1.5f * deltaTime);
-        }
         
-    }
+    } 
 }
---
+++ Prototype3/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveForce = 10.0f;
+    public float jumpForce = 20.0f;
+    public float groundDistance = 0.2f;
+    public LayerMask groundLayers;
+    public Transform groundChecker;
+
+    public float movingDrag = 0.5f;
+    public float normalDrag = 0.9f;
+    public float airDrag = 0.0f;
+
+    public int extraJumpFrames = 2;
+
+    [Range(0.01f, 1.0f)]
+    public float airControl = 0.1f;
+
+    private Rigidbody rigidBody;
+    public Vector3 inputs = Vector3.zero;
+    private bool isGrounded = true;
+    private Camera cam;
+
+    private List<bool> groundedFrames;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        cam = Camera.main;
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        groundedFrames = new List<bool>(extraJumpFrames);
+
+        for (int i = 0; i < extraJumpFrames; i++)
+        {
+            groundedFrames.Add(false);
+        }
+    }
+
+    private void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundChecker.position, groundDistance, groundLayers, QueryTriggerInteraction.Ignore);
+
+        if (groundedFrames.Capacity > 0)
+        {
+            for (int i = groundedFrames.Capacity - 1; i > 0; i--)
+            {
+                groundedFrames[i] = groundedFrames[i - 1];
+            }
+            groundedFrames[0] = isGrounded;
+        }
+
+        var camForward = cam.transform.forward;
+        camForward.y = 0.0f;
+        camForward.Normalize();
+
+        var camRight = cam.transform.right;
+        camRight.y = 0.0f;
+        camRight.Normalize();
+
+        inputs = Vector3.zero;
+        inputs = Input.GetAxis("Horizontal") * camRight + Input.GetAxis("Vertical") * camForward;
+
+        if (!isGrounded)
+        {
+            rigidBody.drag = airDrag;
+        }
+        else if (inputs != Vector3.zero)
+        {
+            rigidBody.drag = movingDrag;
+        }
+        else
+        {
+            rigidBody.drag = normalDrag;
+        }
+
+        if (inputs != Vector3.zero)
+        {
+            transform.forward = inputs.normalized;
+        }
+
+        // Check if player jumped within lenient jump frames
+        bool lenientJump = false;
+
+        for (int i = 0; i < groundedFrames.Capacity; i++)
+        {
+            if (groundedFrames[i]) { lenientJump = true; }
+        }
+
+        if ((Input.GetButtonDown("Jump") && isGrounded) || (Input.GetButtonDown("Jump") && lenientJump))
+        {
+            Jump();
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        float airModifier = 1.0f;
+        if (!isGrounded) { airModifier *= airControl; }
+
+        rigidBody.AddForce(inputs.normalized * moveForce * airModifier * Time.fixedDeltaTime);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundChecker.position, groundDistance);
+    }
+
+    private void Jump()
+    {
+        rigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        ResetGroundedFrames();
+    }
+
+    private void ResetGroundedFrames()
+    {
+        for (int i = 0; i < groundedFrames.Capacity; i++)
+        {
+            groundedFrames[i] = false;
+        }
+    }
+}
