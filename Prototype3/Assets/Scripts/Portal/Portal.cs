96efbc473ff13278d53a4dc3a263bd8b757828ae
StefTheDev <steffthedev@gmail.com>
2019-09-25 16:45:00 +1200

    Added Portal Scripts
    + Portal Base Class
    + Portal Manager

---
+++ Prototype3/Assets/Scripts/Portal/Portal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PortalState
+{
+    LOCKED,
+    UNLOCKED,
+    GATEWAY
+};
+
+public class Portal : MonoBehaviour
+{
+    public PortalState portalState;
+    public Transform location;
+
+    public PortalState Next()
+    {
+        PortalState[] portalStates = (PortalState[])Enum.GetValues(portalState.GetType());
+        int j = Array.IndexOf<PortalState>(portalStates, portalState) + 1;
+        return (portalStates.Length == j) ? portalStates[0] : portalStates[j];
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Player player = collision.transform.GetComponent<Player>();
+        //if(player != null) player.Teleport(location);
+    }
+}
