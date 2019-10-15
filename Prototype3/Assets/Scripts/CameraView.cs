8c7d65e8fc0ea5c8650cd58d5a3672c17de1d40e
Tim Sinclair <timbbby@gmail.com>
2019-10-16 02:03:35 +1300

    Dialogue & Sprites & Cinematics

--- Prototype3/Assets/Scripts/CameraView.cs
+++ Prototype3/Assets/Scripts/CameraView.cs
@@ -13,13 +13,30 @@ public class CameraView : MonoBehaviour
 
     private void Start()
     {
-        cam = GetComponent<CinemachineVirtualCamera>();
+       cam = GetComponent<CinemachineVirtualCamera>();
+       cam.enabled = false;
+       playerRef = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    // Hard cuts
+    public void HardActivateFor(float seconds)
+    {
+        
+        cam.enabled = true;
+        playerRef.SetInputsDisabled(true);
+        transform.DOMove(this.transform.position, seconds).OnComplete(HardDeactivate);
+    }
+
+    private void HardDeactivate()
+    {
         cam.enabled = false;
-        playerRef = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        playerRef.SetInputsDisabled(false);
     }
 
     public void ActivateFor(float seconds)
     {
+        Start();
+
         cam.enabled = true;
 
         playerRef.SetInputsDisabled(true);
